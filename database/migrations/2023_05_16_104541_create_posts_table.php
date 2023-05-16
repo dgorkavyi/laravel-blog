ff272f98a45b40f858e6ece08857255a389f26a3
dgorkavyi <dima.net.dd@gmail.com>
2023-05-16 14:29:52 +0300

    feat/add filament and create models with its migrations for categories and posts

---
+++ database/migrations/2023_05_16_104541_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->string('title', 300);
+            $table->string('slug', 300);
+
+            $table->string('thumbnail', 2000)->nullable();
+            $table->longText('content');
+            
+            $table->boolean('active')->default(false);
+            $table->dateTime('published_at')->nullable();
+            
+            $table->foreignIdFor(User::class, 'user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
